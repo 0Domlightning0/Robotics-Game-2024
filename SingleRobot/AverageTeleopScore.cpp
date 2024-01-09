75468f3261325c03265891c20b1e910cceb85522
Domlightning <99225898+0Domlightning0@users.noreply.github.com>
2024-01-09 12:52:41 -0800

    Create AverageTeleopScore.cpp

---
+++ SingleRobot/AverageTeleopScore.cpp
@@ -0,0 +1,139 @@
+// Game simulation of 2024 Crecendo
+// FRC 2609
+// FRC2024_GameSim.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <time.h>
+#include <random>
+#include <windows.h>
+
+using namespace std; 
+
+
+class Robot {
+public:
+
+	int AvgPickupTime, AvgTravelTime, time = 135, Count = 0, Pcount = 0, PointsScored = 0, PiecesScored = 0; 
+
+	bool HaveRing = false, AtSource = false;
+};
+
+// Robot that only Shoots in the speaker
+class Shooter: public Robot{
+public:
+	int AvgShootingTime, Accuracy, Scount = 0, Consistancy;
+
+	bool ShootingR = false, Amped = false;
+
+	int ShooterGameSim() {
+
+		// Creates a Random shooting Time 
+		// ( (random number % range) + Variable )
+		int RandShootingTime = (((rand() % (7 - Consistancy)) + AvgShootingTime + (5 - Consistancy)));
+
+		int RandTravelTime = (((rand() % (7 - Consistancy)) + AvgShootingTime + (5 - Consistancy)));
+
+		int RandPickupTime = (((rand() % (7 - Consistancy)) + AvgShootingTime + (5 - Consistancy)));
+
+		PointsScored = 0;
+		Count = 0;
+		Scount = 0;
+		ShootingR = false;
+		HaveRing = false;
+		AtSource = false;
+
+		//
+
+
+
+		for (int i = 0; i < time; i++) {
+
+			int RandShootingTime = ((rand() % (7 - Consistancy) + AvgShootingTime));
+
+
+
+			Count += 1;
+
+			// Travelling to Source
+			if (Count >= RandTravelTime and HaveRing == false) {
+				AtSource = true;
+				
+			}
+
+			// Picking Up Piece
+			if (AtSource == true and HaveRing == false) {
+				//cout << "Picking up" << endl;
+				Pcount += 1;
+				
+				
+			}
+			// Picking up from Source
+			if (Pcount >= RandPickupTime and HaveRing == false) {
+				//cout << "Picked up!" << endl;
+				HaveRing = true;
+				AtSource = false;
+				Count = 0;
+				Pcount = 0;
+			}
+
+	        // Travelling back and getting ready to Shoot
+			if (Count >= RandTravelTime and HaveRing == true) {
+				ShootingR = true;
+			}
+
+			// Ready to Shoot
+			if (ShootingR == true) {
+				//cout << "Preparing to Shoot" << endl;
+				Scount += 1;
+			}
+
+			// Aiming and Shooting 
+			if (Scount >= RandShootingTime) {
+				//cout << "Shot Fired!" << endl;
+				PointsScored += 2;
+				PiecesScored += 1;
+				Count = 0;
+				Scount = 0;
+				ShootingR = false;
+				HaveRing = false;
+			}
+
+			if (ShootingR == false and AtSource == false) {
+				//cout << "Driving" << endl;
+			}
+
+			//Sleep(100);
+		}
+		return PointsScored;
+	}
+};
+
+class AmpScorer : public Robot {
+
+	int AvgScoringTime, Accuracy;
+};
+
+
+int main()
+{
+	srand(time(NULL));
+
+	Shooter Shooter1;
+	Shooter1.AvgPickupTime = 3;
+	Shooter1.AvgShootingTime = 3;
+	Shooter1.AvgTravelTime = 6;
+	Shooter1.Consistancy = 3;
+	cout << "This bot Scored:" << Shooter1.ShooterGameSim() << endl;
+	cout << Shooter1.PiecesScored << endl;
+
+
+	double Total = 0;
+
+	for (int i = 0; i < 1000; i++) {
+		Total += Shooter1.ShooterGameSim();
+		cout << Total << endl;
+	}
+	cout << (Total/1000);
+}
+
